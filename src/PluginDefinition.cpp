1c52558c52a03238dca2ab00b0ecd2f35974a4d8
berkay <berkayozdemir764@gmail.com>
2025-12-07 12:40:27 +0300

    stack added for mapping the paranthesis and identifiers

--- src/PluginDefinition.cpp
+++ src/PluginDefinition.cpp
@@ -1,6 +1,8 @@
 #include "PluginDefinition.h"
 #include "menuCmdID.h"
 #include <string>
+#include <stack>
+#include <unordered_map>
 
 FuncItem funcItem[nbFunc];
 
@@ -16,7 +18,7 @@ void pluginCleanUp()
 
 void commandMenuInit()
 {
-    setCommand(0, TEXT("Format Script"), formatScript, false);
+	setCommand(0, TEXT("Format Script"), formatScript, false);
 }
 
 void commandMenuCleanUp()
@@ -24,99 +26,151 @@ void commandMenuCleanUp()
 }
 
 
-bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk, bool check0nInit) 
+bool setCommand(size_t index, TCHAR* cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey* sk, bool check0nInit)
 {
-    if (index >= nbFunc)
-        return false;
+	if (index >= nbFunc)
+		return false;
 
-    if (!pFunc)
-        return false;
+	if (!pFunc)
+		return false;
 
-    lstrcpy(funcItem[index]._itemName, cmdName);
-    funcItem[index]._pFunc = pFunc;
-    funcItem[index]._init2Check = check0nInit;
-    funcItem[index]._pShKey = sk;
+	lstrcpy(funcItem[index]._itemName, cmdName);
+	funcItem[index]._pFunc = pFunc;
+	funcItem[index]._init2Check = check0nInit;
+	funcItem[index]._pShKey = sk;
 
-    return true;
+	return true;
 }
 //---------------------//
 //-- HELPER METHODS --//
 //-------------------//
 HWND getCurrentScintilla()
 {
-    int which = 0; 
-    ::SendMessage(nppData._nppHandle, NPPM_GETCURRENTSCINTILLA, 0, (LPARAM)&which);
+	int which = 0;
+	::SendMessage(nppData._nppHandle, NPPM_GETCURRENTSCINTILLA, 0, (LPARAM)&which);
 
-    return (which == 0) ? nppData._scintillaMainHandle
-        : nppData._scintillaSecondHandle;
+	return (which == 0) ? nppData._scintillaMainHandle
+		: nppData._scintillaSecondHandle;
 }
 
 std::string getAllText()
 {
-    HWND cur = getCurrentScintilla();
-    if (!cur)
-        return "";
+	HWND cur = getCurrentScintilla();
+	if (!cur)
+		return "";
 
-    int len = (int)::SendMessage(cur, SCI_GETTEXTLENGTH, 0, 0);
+	int len = (int)::SendMessage(cur, SCI_GETTEXTLENGTH, 0, 0);
 
-    std::string text;
-    text.resize(len + 1);
+	std::string text;
+	text.resize(len + 1);
 
-    ::SendMessage(cur, SCI_GETTEXT, len + 1, (LPARAM)text.data());
+	::SendMessage(cur, SCI_GETTEXT, len + 1, (LPARAM)text.data());
 
-    return text;
+	return text;
+}
+
+std::string createMapping(const std::string& input) {
+	std::stack<int> identifierStack;
+	std::stack<int> paranthesisStack;
+
+	std::unordered_map<int, int> pairIdentifierMap;
+	std::unordered_map<int, int> pairParanthesisMap;
+
+	int curIndex = 0;
+	for (char c : input)
+	{
+		if (c == '?')
+		{
+			identifierStack.push(curIndex);
+		}
+		else if (c == ':')
+		{
+			pairIdentifierMap[identifierStack.top()] = curIndex;
+			identifierStack.pop();
+
+		}
+		else if (c == '(')
+		{
+			paranthesisStack.push(curIndex);
+		}
+		else if (c == ')') {
+			pairParanthesisMap[paranthesisStack.top()] = curIndex;
+			paranthesisStack.pop();
+		}
+		curIndex++;
+	}
 }
 std::string formatTernary(const std::string& input)
 {
-    std::string out;
-    int indent = 0;
+	std::string out;
+	int indent = 0;
+	int index = -1;
 
-    auto emitIndent = [&out, &indent]()
-        {
-            out.append(indent * 4, ' '); 
-        };
+	auto emitIndent = [&out, &indent]()
+		{
+			out.append(indent * 4, ' ');
+		};
 
-    for (char c : input)
-    {
-        if (c == '?')
-        {
-            out += " ?";
+	for (char c : input)
+	{
+		bool doIntend = false;
+		index++;
+		if (c == '?')
+		{
+			if (index != 0 && input[index - 1] != ' ') {
+				out += " ";
+			}
 
-            out += '\n';
-            ++indent;
-            emitIndent();
-        }
-        else if (c == ':')
-        {
-            out += '\n';
-            if (indent > 0)
-                --indent;
-            emitIndent();
-            out += ": ";
-        }
-        else
-        {
-            out += c;
-        }
-    }
+			out += "?";
 
-    return out;
+			if (index != input.length() - 1 && input[index + 1] != '\n') {
+				out += '\n';
+				doIntend = true;
+			}
+
+			if (doIntend) {
+				++indent;
+				emitIndent();
+			}
+		}
+		else if (c == ':')
+		{
+			if (index != 0 && input[index - 1] != '\n') {
+				out += '\n';
+				doIntend = true;
+			}
+			if (indent > 0)
+				--indent;
+			if (doIntend)
+				emitIndent();
+			out += ":";
+			if (index != input.length() - 1 && input[index + 1] != ' ') {
+				out += " ";
+			}
+		}
+		else
+		{
+			out += c;
+		}
+	}
+
+	return out;
 }
 //---------------------------//
 //-- ASSOCIATED FUNCTIONS --//
 //-------------------------//
 void formatScript()
 {
-    std::string text = getAllText();
-    std::string formatted = formatTernary(text);
+	std::string text = getAllText();
+	std::string formatted = formatTernary(text);
 
-    HWND cur = getCurrentScintilla();
-    if (!cur)
-        return;
+	HWND cur = getCurrentScintilla();
+	if (!cur)
+		return;
 
-    ::SendMessage(cur, SCI_BEGINUNDOACTION, 0, 0);
-    ::SendMessage(cur, SCI_SETTEXT, 0, (LPARAM)formatted.c_str());
-    ::SendMessage(cur, SCI_ENDUNDOACTION, 0, 0);
+	::SendMessage(cur, SCI_BEGINUNDOACTION, 0, 0);
+	::SendMessage(cur, SCI_SETTEXT, 0, (LPARAM)formatted.c_str());
+	::SendMessage(cur, SCI_ENDUNDOACTION, 0, 0);
 }
 
 
